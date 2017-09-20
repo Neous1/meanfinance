a3c63ff66408f42ac5171f74355251ff0c3439fb
Ara <ayaghsizian@gmail.com>
2017-09-20 13:23:47 -0400

    stockprice variable in getPriceFactory is correctly getting price

--- public/angular-app/factories/getPriceFactory.js
+++ public/angular-app/factories/getPriceFactory.js
@@ -1,29 +1,32 @@
 angular.module('cdfinance').factory('getPriceFactory', getPriceFactory);
 
-function getPriceFactory(symbol) {
+function getPriceFactory($http) {
   // var vm = this;
 
   return {
     findPrice: findPrice
   };
 
-  function findPrice(){
-
+  function findPrice(symbol){
+    // var x = 3;
     // // symbol can be put in input as lower or uppercase
     // var symbol = getPriceFactory.symbol.toUpperCase();
     // // symbol will always show as uppercase on browser
     // console.log(symbol)
-    
-    console.log('hi');
 
-    // return $http.get("/api/stocks/" + symbol).then(function(response) {
-    //          console.log("found stock")
-    //          var stockprice = response.data.price
-    //            getPriceFactory.stockprice = stockprice;
-    //          }).catch(function(error) {
-    //            if (error) {
-    //              getPriceFactory.error = error;
-    //            }
-    //          })//catch
-  };//find
+    console.log("sanity check " + symbol);
+
+    return $http.get("/api/stocks/" + symbol).then(complete).catch(failed);
+  };//findPrice
+
+  function complete (response) {
+    console.log("found stock");
+    var stockprice = response.data.price;
+    console.log("this is stockprice: "+stockprice);
+    getPriceFactory.stockprice = stockprice;
+  };//complete
+
+  function failed (error) {
+    console.log(error);
+  }//catch
 }
--- public/angular-app/find/find-controller.js
+++ public/angular-app/find/find-controller.js
@@ -1,26 +1,30 @@
 angular.module('cdfinance').controller("FindController", FindController);
 
-function FindController($http) {
+function FindController($http, getPriceFactory) {
   var vm = this;
 
 
   console.log("findController");
+
   vm.find = function() {
-    // // symbol can be put in input as lower or uppercase
+
+    // // // symbol can be put in input as lower or uppercase
+    var greeting = "hello";
     var symbol = vm.symbol.toUpperCase();
-    // // symbol will always show as uppercase on browser
-    console.log(symbol)
 
-    // getPriceFactory.findPrice();
+    // // // symbol will always show as uppercase on browser
+    // console.log("this is symbol: "+symbol);
 
-    $http.get("/api/stocks/" + symbol).then(function(response) {
-      console.log("found stock")
-      var stockprice = response.data.price
-      vm.stockprice = stockprice;
-    }).catch(function(error) {
-      if (error) {
-        vm.error = error;
-      }
-    })//catch
+    getPriceFactory.findPrice( symbol );
+
+    // $http.get("/api/stocks/" + symbol).then(function(response) {
+    //   console.log("found stock")
+    //   var stockprice = response.data.price
+    //   vm.stockprice = stockprice;
+    // }).catch(function(error) {
+    //   if (error) {
+    //     vm.error = error;
+    //   }
+    // })//catch
   }//find
 }
