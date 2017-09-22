949afdb027211722ca99882c5266efcd86fbea93
Neous1 <Neous1@users.noreply.github.com>
2017-09-22 16:16:53 -0400

    display totalValue on dashboard call totalvalue func
    added totalValue  column

--- public/angular-app/dashboard/dashboard-controller.js
+++ public/angular-app/dashboard/dashboard-controller.js
@@ -12,27 +12,44 @@ function DashboardController($http, $window, AuthFactory, jwtHelper, $location) 
         vm.stocks = response.data.stocks;
 
         vm.stocks.sort(function (a, b) {
-          console.log("18. ", a, b);
-          console.log("18. ", a._id, b._id);
+          // console.log("18. ", a, b);
+          // console.log("18. ", a._id, b._id);
           return (a._id < b._id) ? -1 : (a._id > b._id) ? 1 : 0;
         });
 
         latestprices = [];
       })
-      // .then(function (response) {
-      //   // 14. vm.stock
-      //   console.log('14. vm.stock: ', vm.stocks);
-      //   vm.stocks.forEach(function (element) {
-      //     $http.get("/api/latestprice/" + element._id)
-      //       .then(function (response) {
-      //         console.log("20 . ", response.data.sym, response.data.price)
-      //       })
-      //   }, this);
+      .then(function (response) {
+        // 14. vm.stock
+        console.log('14. vm.stock: ', vm.stocks);
+        vm.stocks.forEach(function (element) {
+          $http.get("/api/latestprice/" + element._id)
+            .then(function (response) {
+              console.log("20 . ", response.data.sym, response.data.price);
+              var resData = response.data;
+              vm.stocks.find(function(item){
+                if (item._id === response.data.sym) {
+                  item["price"]=response.data.price;
+                  console.log("33. ", item);
+                  item["totalValue"]= totalValue(item["price"], item.amount);
+                }
+              })
+            })
+        }, this);
 
-      // })
+      })
       .catch(function (error) {
         console.log(error);
       })
+      function totalValue(price, amount){
+        var val = parseFloat(price) * parseInt(amount);
+        console.log("46 . ", val);
+        return val ;
+
+      }
+
+
+
     $http.get('/api/users/' + username).then(function (response) {
       vm.balance = response.data
     })
